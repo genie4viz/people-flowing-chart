81b50559ee8106718062239dad19a73e3448a3d8
selistym <selistym@protonmail.com>
2019-09-06 13:57:17 +0300

    init for start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import _ from 'lodash';
 import d3 from 'd3';
-import printMe from './print.js';
+import tracks from './csv/total_tracks.csv';
+// import printMe from './print.js';
 
 function component() {
+  
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // const btn = document.createElement('button');
+  // btn.innerHTML = 'Click me and check the console!';
+  // btn.onclick = printMe;
 
-  const btn = document.createElement('button');
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  // element.appendChild(btn);
 
-  element.appendChild(btn);
+  initData(tracks);
+
   return element;
 }
+
+function initData(data) {
+    console.log(data)
+}
+
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,41 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+       {
+         test: /\.(csv|tsv)$/,
+         use: [
+           'csv-loader'
+         ]
+       },
+       {
+         test: /\.xml$/,
+         use: [
+           'xml-loader'
+         ]
+       }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
